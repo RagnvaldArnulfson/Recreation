b65e1c59343ceb57f1ad9fe5ec22d77dca9b6b75
RagnvaldArnulfson <gus683@hotmail.fr>
2015-12-07 19:28:02 +0100

    Create sort.py

---
+++ sort.py
@@ -0,0 +1,45 @@
+import random as r
+
+m=[r.randint(0,99999) for i in range(10**2)]
+
+def quicksort(l):
+   n=len(l)
+   if n==0:
+      return []
+   p=l[0]
+   l1=[e for e in l[1:] if e<p]
+   l2=[e for e in l[1:] if e>=p]
+   return quicksort(l1)+[p]+quicksort(l2)
+
+def bubblesort(l):
+   i=0
+   n=len(l)-1
+   while i<n:
+      if l[i]>l[i+1]:
+         l[i],l[i+1]=l[i+1],l[i]
+         i=-1
+      i+=1
+   return l
+      
+def exchangesort(l):
+   for i in range(len(l)-1):
+      for j in range(i+1,len(l)):
+         if l[i]>l[j]:
+            l[i],l[j]=l[j],l[i]
+   return l
+      
+def selectionsort(l):
+   m=0
+   for i in range(len(l)-1):
+      for j in range(i,len(l)):
+         if l[j]<l[m]:
+            m=j
+      l[m],l[i]=l[i],l[m]
+   return l
+
+
+
+print(selectionsort(m),"\n")
+print(bubblesort(m),"\n")
+print(exchangesort(m),"\n")
+print(quicksort(m))
