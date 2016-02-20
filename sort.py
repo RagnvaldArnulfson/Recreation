3ec4c47d2d2539a3af2956e98e5491ae1a10f44b
RagnvaldArnulfson <gus683@hotmail.fr>
2016-02-20 18:17:01 +0100

    Update sort.py

--- sort.py
+++ sort.py
@@ -9,15 +9,17 @@ def quicksort(l):
    p=l[0]
    return quicksort([e for e in l[1:] if e<p])+[p]+quicksort([e for e in l[1:] if e>=p])
 
-def bubblesort(l):
-   i=0
-   n=len(l)-1
-   while i<n:
-      if l[i]>l[i+1]:
-         l[i],l[i+1]=l[i+1],l[i]
-         i=-1
-      i+=1
-   return l
+def gnomesort(a):
+   i,j,size = 1,2,len(a)
+   while i < size:
+      if a[i-1] <= a[i]:
+         i,j = j, j+1
+      else:
+         a[i-1],a[i] = a[i],a[i-1]
+         i -= 1
+         if i == 0:
+            i,j = j, j+1
+   return a
       
 def exchangesort(l):
    for i in range(len(l)-1):
@@ -35,9 +37,34 @@ def selectionsort(l):
       l[m],l[i]=l[i],l[m]
    return l
 
-
-
-print(selectionsort(m),"\n")
-print(bubblesort(m),"\n")
-print(exchangesort(m),"\n")
-print(quicksort(m))
+def bubblesort(a):
+    fin = len(a)-1#-2 en vrai
+    en_desordre = True
+    while en_desordre:
+        en_desordre = False
+        for i in range(fin):
+            if a[i]>a[i+1]:
+                en_desordre = True
+                a[i],a[i+1]=a[i+1],a[i]
+        fin-=1
+        print(a)
+    return a
+    
+def cocktailshakersort(a):
+   deb =0
+   fin = len(a)-1#-2 en vrai
+   en_desordre = True
+   while en_desordre:
+      en_desordre = False
+      for i in range(deb,fin):
+         if a[i]>a[i+1]:
+            en_desordre = True
+            a[i],a[i+1]=a[i+1],a[i]
+      fin-=1
+      for i in range(fin,deb-1,-1): #pas de -1 en vrai
+         if a[i]>a[i+1]:
+            en_desordre = True
+            a[i],a[i+1]=a[i+1],a[i]
+      deb+=1
+      print(a)
+   return a
